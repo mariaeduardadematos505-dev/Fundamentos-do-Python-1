2705c22255d1e79a6060b53d32ec51ea0049b7aa
mariaeduardadematos505-dev <mariaeduarda.de.matos505@gmail.com>
2025-12-04 16:59:57 -0300

    Add files via upload

---
+++ Aula_2Tipos de dados Python.py
@@ -0,0 +1,358 @@
+# Objetivo mostrar o que está dentro do parenteses dela no
+# no ambiente de teste
+# 4 Tipos strigs float inteiro boleanos
+
+# quebra de linha em Python
+print('isso é\num texto')
+print()
+print('''
+mksdjoisj
+kasjnoiahd
+jandoishdx
+''')
+
+print(10)
+print(10.5)
+print(True, False)
+
+print('Olá seja bem vindo Julia\nObrigada volte sempre')
+print('House\'s')
+print("House's")
+
+print("Meu nome é ", end="-")
+print("Monty Python.")
+
+#Literais
+#Concatenação  separar organizar os dados
+print("nome","uva","teste")
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+
+print("Monty", "Python.", sep="*", end="*\n")
+
+# Calcular
+print(0x123)
+print(1**22)
+
+print('''
+''')
+
+#os computadores sabem apenas dois tipos de respostas:
+
+print("Sim, isso é verdade;")
+print("Não, isso é falso.")
+print('''
+''')
+#Python como calculadora
+#Aritmética - sempre um número como saída***
+print(10 + 2)
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2)
+print(10 // 2)
+print(10 % 2)
+print(10 ** 0.5)
+print('''
+''')
+#Lógico - True ou false
+print(10 > 2) #maior
+print(10 < 2)#menor
+print(10 >= 2)#maior ou igual
+print(10 <= 2)#menor ou igual
+print(10 == 2)#comparação
+print(10 != 2)#diferente
+
+print(14 % 4 == 0)
+
+# Estrutura de dado palavra = guardando um dado
+# Dinamicamente
+#unica
+#crie
+variavel = 5
+Variavel = 10
+VARIAVEL =20
+print(20)
+print(VARIAVEL)
+
+
+
+_teste = 10
+
+# estrutura de dado  palavra  =
+# fracamente tipadas
+# dinamicamente
+# únicas
+# CRIE
+# atribuição
+# 1  dado
+
+numero_1 = 10
+numero_2 = 20
+soma  =  numero_1 + numero_2
+print(soma)
+
+altura  = 1.70
+peso =  70
+imc  =  peso/(altura ** 2)
+print('IMC:', imc)
+
+
+nome     = 'José'
+idade    =  35
+endereco =  'Rua 10, jd x' 
+email =  'jose@gmail.com'
+print('Nome:', nome)
+print('Idade:',idade)
+print('Endereço:', endereco)
+print('E-mail:',email)
+
+print(f'''
+Nome: {nome}
+Idade: {idade}
+Endereço:{endereco}
+E-mail:{email}
+''')
+
+# Objetivo mostrar o que está dentro do parenteses dela no
+# no ambiente de teste
+# 4 Tipos strigs float inteiro boleanos
+
+# quebra de linha em Python
+print('isso é\num texto')
+print()
+print('''
+mksdjoisj
+kasjnoiahd
+jandoishdx
+''')
+
+print(10)
+print(10.5)
+print(True, False)
+
+print('Olá seja bem vindo Julia\nObrigada volte sempre')
+print('House\'s')
+print("House's")
+
+print("Meu nome é ", end="-")
+print("Monty Python.")
+
+#Literais
+#Concatenação  separar organizar os dados
+print("nome","uva","teste")
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+
+print("Monty", "Python.", sep="*", end="*\n")
+
+# Calcular
+print(0x123)
+print(1**22)
+
+print('''
+''')
+
+#os computadores sabem apenas dois tipos de respostas:
+
+print("Sim, isso é verdade;")
+print("Não, isso é falso.")
+print('''
+''')
+#Python como calculadora
+#Aritmética - sempre um número como saída***
+print(10 + 2)
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2)
+print(10 // 2)
+print(10 % 2)
+print(10 ** 0.5)
+print('''
+''')
+#Lógico - True ou false
+print(10 > 2) #maior
+print(10 < 2)#menor
+print(10 >= 2)#maior ou igual
+print(10 <= 2)#menor ou igual
+print(10 == 2)#comparação
+print(10 != 2)#diferente
+
+print(14 % 4 == 0)
+
+# Estrutura de dado palavra = guardando um dado
+# Dinamicamente
+#unica
+#crie
+variavel = 5
+Variavel = 10
+VARIAVEL =20
+print(20)
+print(VARIAVEL)
+
+
+
+_teste = 10
+
+# estrutura de dado  palavra  =
+# fracamente tipadas
+# dinamicamente
+# únicas
+# CRIE
+# atribuição
+# 1  dado
+
+numero_1 = 10
+numero_2 = 20
+soma  =  numero_1 + numero_2
+print(soma)
+
+altura  = 1.70
+peso =  70
+imc  =  peso/(altura ** 2)
+print('IMC:', imc)
+
+
+nome     = 'José'
+idade    =  35
+endereco =  'Rua 10, jd x' 
+email =  'jose@gmail.com'
+print('Nome:', nome)
+print('Idade:',idade)
+print('Endereço:', endereco)
+print('E-mail:',email)
+
+print(f'''
+Nome: {nome}
+Idade: {idade}
+Endereço:{endereco}
+E-mail:{email}
+''')
+
+# Objetivo mostrar o que está dentro do parenteses dela no
+# no ambiente de teste
+# 4 Tipos strigs float inteiro boleanos
+
+# quebra de linha em Python
+print('isso é\num texto')
+print()
+print('''
+mksdjoisj
+kasjnoiahd
+jandoishdx
+''')
+
+print(10)
+print(10.5)
+print(True, False)
+
+print('Olá seja bem vindo Julia\nObrigada volte sempre')
+print('House\'s')
+print("House's")
+
+print("Meu nome é ", end="-")
+print("Monty Python.")
+
+#Literais
+#Concatenação  separar organizar os dados
+print("nome","uva","teste")
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+
+print("Monty", "Python.", sep="*", end="*\n")
+
+# Calcular
+print(0x123)
+print(1**22)
+
+print('''
+''')
+
+#os computadores sabem apenas dois tipos de respostas:
+
+print("Sim, isso é verdade;")
+print("Não, isso é falso.")
+print('''
+''')
+#Python como calculadora
+#Aritmética - sempre um número como saída***
+print(10 + 2)
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2)
+print(10 // 2)
+print(10 % 2)
+print(10 ** 0.5)
+print('''
+''')
+#Lógico - True ou false
+print(10 > 2) #maior
+print(10 < 2)#menor
+print(10 >= 2)#maior ou igual
+print(10 <= 2)#menor ou igual
+print(10 == 2)#comparação
+print(10 != 2)#diferente
+
+print(14 % 4 == 0)
+
+# Estrutura de dado palavra = guardando um dado
+# Dinamicamente
+#unica
+#crie
+variavel = 5
+Variavel = 10
+VARIAVEL =20
+print(20)
+print(VARIAVEL)
+
+
+
+_teste = 10
+
+# estrutura de dado  palavra  =
+# fracamente tipadas
+# dinamicamente
+# únicas
+# CRIE
+# atribuição
+# 1  dado
+
+numero_1 = 10
+numero_2 = 20
+soma  =  numero_1 + numero_2
+print(soma)
+
+altura  = 1.70
+peso =  70
+imc  =  peso/(altura ** 2)
+print('IMC:', imc)
+
+
+nome     = 'José'
+idade    =  35
+endereco =  'Rua 10, jd x' 
+email =  'jose@gmail.com'
+print('Nome:', nome)
+print('Idade:',idade)
+print('Endereço:', endereco)
+print('E-mail:',email)
+
+print(f'''
+Nome: {nome}
+Idade: {idade}
+Endereço:{endereco}
+E-mail:{email}
+''')
+
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=',')
+
+total_apples = john + mary + adam
+print(total_apples)
+
+# peter = 12.5
+# suzy = 2
+# print(peter / suzy)
+# print("Número total de maçãs:", total_apples)
