d2c356477d48c68e80ff488ccda9560acc9cca88
Noir Matos <mariaeduarda.de.matos505@gmail.com>
2025-12-11 16:37:04 -0300

    Add files via upload

---
+++ aula_3.py
@@ -0,0 +1,49 @@
+numero = float(input("digite um numero real:"))
+print(f"O numero digitado foi: {numero}")
+
+cubo = numero ** 3
+
+print(f"O cubo de {numero} é : {cubo}")
+
+### cubo do número
+# input() -> pede informação -> stdin 
+# print() -> mostra informação -> stdout
+
+numero = float(input("digite um numero real:"))
+
+print(f"O numero digitado foi: {type(numero)}")
+
+cubo = numero ** 3
+
+print(f"O cubo de {numero} é : {cubo}")
+
+# #criando variavel para fruta
+
+fruta_1 = "Morango"
+fruta_2 = "Maça"
+fruta_3 = "laranja"
+
+lista_de_frutas = fruta_1 + ", "+ fruta_2 + " e " + fruta_3
+print("Frutas", lista_de_frutas)
+
+## Concatenção x Soma
+fru1_str = float(input("Digite o valor da fru1: "))
+fru2_str = float(input("Digite o valor da fru2: "))
+fru3_str = float(input("Digite o valor da fru3: "))
+
+soma = fru1_str + fru2_str + fru3_str
+soma = fru1_str + fru2_str + fru3_str
+
+print(f'A soma é: {soma}')
+
+fru1_str = input("Digite o valor da fru1: ")
+fru2_str = input("Digite o valor da fru2: ")
+fru3_str = input("Digite o valor da fru3: ")
+
+concatencao = fru1_str + fru2_str + fru3_str
+concatencao = fru1_str + fru2_str + fru3_str
+
+print(f'A concatencao é: {concatencao}')
+
+print('Python é show!')
+
